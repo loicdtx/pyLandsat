39c7d644a7c6f15faaaf754f98a82d6168850e38
Loïc <loic.dutrieux@gmail.com>
2015-12-21 15:50:26 -0800

    Experimemting with ways to crop and clean MODIS data

--- modisDownload.py
+++ modisDownload.py
@@ -1,6 +1,7 @@
 from osgeo import gdal
 from pymodis import downmodis
 import glob
+import numpy as np
 
 dest = "/home/dutri001/sandbox/MODIS/"
 tiles = "h18v03"
@@ -16,11 +17,50 @@ modis_down.downloadsAllDay()
 # List files with the hdf extension
 MODIS_file = glob.glob(dest + '*.hdf')
 
-# 
-sds = gdal.Open(MODIS_file[0]).GetSubDatasets()
+# # 
+sds = gdal.Open(MODIS_file[0], gdal.GA_ReadOnly).GetSubDatasets()
+print sds[0][0]
+print sds[11][0]
 vi = gdal.Open(sds[0][0])
-vi_np = vi.ReadAsArray()
-print vi_np
+ind = [1, 11]
+# vi_np = np.zeros((vi.RasterYSize, vi.RasterXSize, 2), np.int16)
+vi_np_sub = np.zeros((200, 200, 2), np.int16)
+for i in range(len(ind)):
+	src = gdal.Open(sds[ind[i]][0])
+	vi_np_sub[:,:,i] = src.ReadAsArray(400, 400, 200, 200)
+
+print vi_np_sub.shape
+print vi.GetGeoTransform()
+
+# Get Geotransforms of original dataset (vi.GetGeoTransform())
+# Modify the list of geotransforms according to the array subset
+# Set modified GeoTransform to new dataset
+# See http://gis.stackexchange.com/questions/58517/python-gdal-save-array-as-raster-with-projection-from-other-file
+
+
+
+
+# driver = gdal.GetDriverByName('ENVI')
+# driver.Register()
+# outDataset = driver.Create('ERS1PRI_19920430_ENVI_subset', 50, 50, 1, gdal.GDT_Float32) 
+
+
+# outBand = outDataset.GetRasterBand(1)
+# outBand.WriteArray(subset, 0, 0)
+# band = None
+# dataset = None
+# outDataset = None
+# subset = None
+
+
+
+
+
+
+
+
+
+
 
 
 
